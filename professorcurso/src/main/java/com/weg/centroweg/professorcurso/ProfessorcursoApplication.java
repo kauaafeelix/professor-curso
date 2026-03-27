7f4f0cf2a4a55b04ef3766a08aaea3cbb5fba4af
Kaua Felix <kfelixcosta11@gmail.com>
2026-03-27 16:30:44 -0300

    chore: adiciona configuracoes iniciais do projeto

---
+++ professorcurso/src/main/java/com/weg/centroweg/professorcurso/ProfessorcursoApplication.java
@@ -0,0 +1,13 @@
+package com.weg.centroweg.professorcurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorcursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorcursoApplication.class, args);
+	}
+
+}
---
+++ professorcurso/src/test/java/com/weg/centroweg/professorcurso/ProfessorcursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.centroweg.professorcurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorcursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
