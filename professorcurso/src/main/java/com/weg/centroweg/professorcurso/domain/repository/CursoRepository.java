1570b31ab8a3ef0f401a6fcda179806b6173c18f
Kaua Felix <kfelixcosta11@gmail.com>
2026-03-27 17:00:04 -0300

    fix: corrige tipo de retorno do metodo findByIdAndNome

--- professorcurso/src/main/java/com/weg/centroweg/professorcurso/domain/repository/CursoRepository.java
+++ professorcurso/src/main/java/com/weg/centroweg/professorcurso/domain/repository/CursoRepository.java
@@ -4,11 +4,12 @@ import com.weg.centroweg.professorcurso.domain.entity.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CursoRepository extends JpaRepository<Curso, Long> {
     List<Curso> findByProfessor_Nome(String professorNome);
 
     List<Curso> findByProfessorId(Long professorId);
 
-    Curso findByIdAndNome(Long id, String nome);
+    Optional<Curso> findByIdAndNome(Long id, String nome);
 }
